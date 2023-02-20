b2e0ea007895e0b6acc128c0fb9381898cdeeb18
whrudwls08 <whrudwls08@naver.com>
2023-02-20 22:03:42 +0900

    change study contents

--- src/Memoization/Memo.jsx
+++ src/Memoization/Memo.jsx
@@ -1,28 +1,29 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import Comments from "./Comments";
 
 const commentList = [
-  { title: 'comment1', content: "message1", likes: 1},
-  { title: 'comment2', content: "message2", likes: 23},
-  { title: 'comment3', content: "message3", likes: 3}
-]
+  { title: "comment1", content: "message1", likes: 1 },
+  { title: "comment2", content: "message2", likes: 23 },
+  { title: "comment3", content: "message3", likes: 3 },
+];
 
-export default function Memo(){
-  const [comments, setComments] = useState(commentList)
-  useEffect(()=>{
-    const interval = setInterval(()=>{
-      setComments((prevComments)=>[
+export default function Memo() {
+  const [comments, setComments] = useState(commentList);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setComments((prevComments) => [
         ...prevComments,
         {
-           title: `${prevComments.length + 1}`, content: "message3", likes: 3
-        }
-      ])
+          title: `${prevComments.length + 1}`,
+          content: "message3",
+          likes: 3,
+        },
+      ]);
     }, 1000);
 
-    return (()=>{
-      clearInterval(interval)
-    })
-
-  })
-  return <Comments commentList={comments}/>
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  // return <Comments commentList={comments}/>
 }
